c7feb07e27c93593658c5340790598a497fb3cdd
Rowdenn <allanb.59@live.fr>
2025-07-21 20:14:57 +0200

    Ajout du stream du personnage dans l'inventaire

--- r_inventory/client/cl_inventory.lua
+++ r_inventory/client/cl_inventory.lua
@@ -14,7 +14,7 @@ function OpenInventory()
     local playerPed = PlayerPedId()
     SetEntityAlpha(playerPed, 100, false)
     
-    -- CreatePedScreen()
+    CreatePedScreen()
 
     -- Récupérer les données nécessaires
     RefreshInventory()
@@ -38,7 +38,7 @@ function CloseInventory()
     
     InventoryOpen = false
 
-    -- CleanupPedScreen()
+    CleanupPedScreen()
     
     SetNuiFocus(false, false)
     
@@ -114,11 +114,8 @@ function SpawnGroundItem(itemData)
     if HasModelLoaded(hash) then
         local object = CreateObject(hash, itemData.x, itemData.y, itemData.z, false, true, true)
         SetEntityAsMissionEntity(object, true, true)
-        
-        -- Faire rebondir l'objet légèrement
         SetEntityVelocity(object, 0.0, 0.0, 0.1)
         
-        -- Optionnel : Ajouter une légère rotation aléatoire
         local randomRotation = math.random(0, 360)
         SetEntityRotation(object, 0.0, 0.0, randomRotation, 2, true)
 
@@ -133,81 +130,80 @@ function RemoveGroundItem(itemId)
     end
 end
 
--- function CreatePedScreen()
---     CreateThread(function()
---         -- Nettoyer l'ancien ped s'il existe
---         if DoesEntityExist(PlayerPedPreview) then
---             DeleteEntity(PlayerPedPreview)
---             PlayerPedPreview = nil
---         end
+function CreatePedScreen()
+    CreateThread(function()
+        -- Nettoyer l'ancien ped s'il existe
+        if DoesEntityExist(PlayerPedPreview) then
+            DeleteEntity(PlayerPedPreview)
+            PlayerPedPreview = nil
+        end
+
+        SetFrontendActive(true)
+        ActivateFrontendMenu(GetHashKey("FE_MENU_VERSION_EMPTY"), true, -1)
+        SetMouseCursorVisible(false)
+        FrontendActive = true
+
+        Citizen.Wait(100)
+
+        PlayerPedPreview = ClonePed(PlayerPedId(), false, true, false)
         
---         local heading = GetEntityHeading(PlayerPedId())
-
---         SetFrontendActive(true)
---         ActivateFrontendMenu(GetHashKey("FE_MENU_VERSION_EMPTY_NO_BACKGROUND"), true, -1)
---         frontendActive = true
-
---         Citizen.Wait(100)
-
---         N_0x98215325a695e78a(false)
-
---         PlayerPedPreview = ClonePed(PlayerPedId(), heading, true, false)
-        
---         if DoesEntityExist(PlayerPedPreview) then
---             local x, y, z = table.unpack(GetEntityCoords(PlayerPedPreview))
+        if DoesEntityExist(PlayerPedPreview) then
+            local x, y, z = table.unpack(GetEntityCoords(PlayerPedPreview))
             
---             SetEntityCoords(PlayerPedPreview, x, y, z - 10)
---             FreezeEntityPosition(PlayerPedPreview, true)
---             SetEntityVisible(PlayerPedPreview, false, false)
---             NetworkSetEntityInvisibleToNetwork(PlayerPedPreview, false)
+            SetEntityCoords(PlayerPedPreview, x, y, z - 100, false, false, false, false)
+            FreezeEntityPosition(PlayerPedPreview, true)
+            SetEntityVisible(PlayerPedPreview, false, false)
+            NetworkSetEntityInvisibleToNetwork(PlayerPedPreview, false)
 
---             Citizen.Wait(200)
+            Citizen.Wait(200)
 
---             SetPedAsNoLongerNeeded(PlayerPedPreview)
---             GivePedToPauseMenu(PlayerPedPreview, 2)
---             SetPauseMenuPedLighting(true)
---             SetPauseMenuPedSleepState(true)
---         end
---     end)
--- end
+            SetPedAsNoLongerNeeded(PlayerPedPreview)
+            GivePedToPauseMenu(PlayerPedPreview, 1)
+            ReplaceHudColourWithRgba(117, 0, 0, 0, 0)
+            SetPauseMenuPedLighting(true)
+            SetPauseMenuPedSleepState(true)
+        end
+    end)
+end
 
--- function CleanupPedScreen()
---     if DoesEntityExist(PlayerPedPreview) then
---         DeleteEntity(PlayerPedPreview)
---         PlayerPedPreview = nil
---     end
+function CleanupPedScreen()
+    if DoesEntityExist(PlayerPedPreview) then
+        DeleteEntity(PlayerPedPreview)
+        PlayerPedPreview = nil
+    end
     
---     if frontendActive then
---         SetFrontendActive(false)
---         RestartFrontendMenu(GetHashKey("FE_MENU_VERSION_EMPTY_NO_BACKGROUND"), -1)
---         frontendActive = false
---     end
--- end
+    if FrontendActive then
+        SetMouseCursorVisible(true)
+        SetFrontendActive(false)
+        RestartFrontendMenu(GetHashKey("FE_MENU_VERSION_EMPTY_NO_BACKGROUND"), -1)
+        FrontendActive = false
+    end
+end
 
--- function UpdateCharacterDisplay()
---     if DoesEntityExist(PlayerPedPreview) then
---         local heading = GetEntityHeading(PlayerPedPreview)
+function UpdateCharacterDisplay()
+    if DoesEntityExist(PlayerPedPreview) then
+        local heading = GetEntityHeading(PlayerPedPreview)
         
---         -- Supprimer l'ancien ped
---         DeleteEntity(PlayerPedPreview)
+        -- Supprimer l'ancien ped
+        DeleteEntity(PlayerPedPreview)
         
---         -- Créer le nouveau ped
---         PlayerPedPreview = ClonePed(PlayerPedId(), heading, true, false)
+        -- Créer le nouveau ped
+        PlayerPedPreview = ClonePed(PlayerPedId(), heading, true, false)
         
---         if DoesEntityExist(PlayerPedPreview) then
---             local x, y, z = table.unpack(GetEntityCoords(PlayerPedPreview))
+        if DoesEntityExist(PlayerPedPreview) then
+            local x, y, z = table.unpack(GetEntityCoords(PlayerPedPreview))
             
---             SetEntityCoords(PlayerPedPreview, x, y, z - 10)
---             FreezeEntityPosition(PlayerPedPreview, true)
---             SetEntityVisible(PlayerPedPreview, false, false)
---             NetworkSetEntityInvisibleToNetwork(PlayerPedPreview, false)
+            SetEntityCoords(PlayerPedPreview, x, y, z)
+            FreezeEntityPosition(PlayerPedPreview, true)
+            SetEntityVisible(PlayerPedPreview, false, false)
+            NetworkSetEntityInvisibleToNetwork(PlayerPedPreview, false)
             
---             Citizen.Wait(200)
+            Citizen.Wait(200)
             
---             SetPedAsNoLongerNeeded(PlayerPedPreview)
---             GivePedToPauseMenu(PlayerPedPreview, 2)
---             SetPauseMenuPedLighting(true)
---             SetPauseMenuPedSleepState(true)
---         end
---     end
--- end
+            SetPedAsNoLongerNeeded(PlayerPedPreview)
+            GivePedToPauseMenu(PlayerPedPreview, 2)
+            SetPauseMenuPedLighting(true)
+            SetPauseMenuPedSleepState(true)
+        end
+    end
+end
