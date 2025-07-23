ef42ac16d79552ea41ac90508da223fbf15c9cdd
Rowdenn <allanb.59@live.fr>
2025-07-23 14:20:40 +0200

    Suppression de la roue des armes

--- framework/client/main.lua
+++ framework/client/main.lua
@@ -1,26 +1,6 @@
--- Ajouter ceci au début du fichier
-local PlayerLoaded = false
+-- Supprime totalement la roue des armes
+while true do
+    Citizen.Wait(0)
 
--- Événement de connexion
-AddEventHandler('playerSpawned', function()
-    if not PlayerLoaded then
-        -- Demander la vérification du personnage
-        TriggerServerEvent('framework:server:CheckCharacterExists')
-    end
-end)
-
--- Événement de joueur chargé
-RegisterNetEvent('framework:client:PlayerLoaded')
-AddEventHandler('framework:client:PlayerLoaded', function(PlayerData)
-    Framework.PlayerData = PlayerData
-    PlayerLoaded = true
-    
-    -- Déclencher les événements de démarrage
-    TriggerEvent('framework:client:OnPlayerLoaded')
-    
-    -- Charger la position
-    if PlayerData.position and PlayerData.position.x then
-        SetEntityCoords(PlayerPedId(), PlayerData.position.x, PlayerData.position.y, PlayerData.position.z)
-        SetEntityHeading(PlayerPedId(), PlayerData.position.heading or 0.0)
-    end
-end)
+    BlockWeaponWheelThisFrame()
+end
