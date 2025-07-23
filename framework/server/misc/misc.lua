1bd96f3da9ae773bb9872ba3974d3dc28ae5dbeb
Rowdenn <allanb.59@live.fr>
2025-07-23 14:20:27 +0200

    Suppression fonctions inutiles

--- framework/server/misc/misc.lua
+++
@@ -1,9 +0,0 @@
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-        
-        -- Désactiver l'affichage du HUD des armes
-        HideHudComponentThisFrame(2) -- WEAPON_WHEEL
-        HideHudComponentThisFrame(20) -- WEAPON_WHEEL_STATS
-    end
-end)
