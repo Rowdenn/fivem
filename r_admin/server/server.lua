9e2ee82ca34ea99a69cb74f239e193c1848dc96c
Rowdenn <allanb.59@live.fr>
2025-07-26 14:44:33 +0200

    Kick

--- r_admin/server/server.lua
+++ r_admin/server/server.lua
@@ -1,11 +1,23 @@
+RegisterServerEvent('r_admin:kickPlayer')
+AddEventHandler('r_admin:kickPlayer', function(target, reason)
+    local admin = source
+
+    -- TODO: Ajouter la logique de kick
+    exports['r_notify']:ShowNotificationToPlayer({
+        message = "Joueur " .. target .. " kick avec succès",
+        type = "success",
+        duration = 5000
+    })
+end)
+
 RegisterCommand('car', function(source, args)
     local player = source
 
     if not args[1] then
         TriggerClientEvent('chat:addMessage', player, {
-            color = {255, 0, 0},
+            color = { 255, 0, 0 },
             muiltine = true,
-            args = {'Utilisation: /car <nom_du_vehicule)'}
+            args = { 'Utilisation: /car <nom_du_vehicule)' }
         })
     end
 
@@ -14,7 +26,7 @@ RegisterCommand('car', function(source, args)
     TriggerClientEvent('r_admin:spawnVehicle', player, vehicleName)
 end, false)
 
-RegisterCommand('fix', function(source, args)
+RegisterCommand('fix', function(source)
     local player = source
-    TriggerClientEvent('r_admin:fixVehicle', player, vehicleName)
+    TriggerClientEvent('r_admin:fixVehicle', player)
 end, false)
