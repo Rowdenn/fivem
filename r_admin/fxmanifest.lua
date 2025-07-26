574324fe3064fec3458a96af3df360c508f8c49b
Rowdenn <allanb.59@live.fr>
2025-07-26 14:42:43 +0200

    Ajout dépendances

--- r_admin/fxmanifest.lua
+++ r_admin/fxmanifest.lua
@@ -5,10 +5,21 @@ author 'Rowden'
 description 'Admin System'
 version '1.0.0'
 
+shared_script "shared/config.lua"
+
 client_scripts {
+    'config.lua',
+    '@menuv/menuv.lua',
     'client/*.lua'
 }
 
 server_scripts {
+    'config.lua',
     'server/*.lua'
+}
+
+dependencies {
+    'framework',
+    'menuv',
+    'r_notify'
 }
