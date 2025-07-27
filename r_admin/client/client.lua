bfab4ddf0932555c0b9a75e50ba88e3a52659d2a
Rowdenn <allanb.59@live.fr>
2025-07-27 20:07:34 +0200

    Clear fichier

--- r_admin/client/client.lua
+++ r_admin/client/client.lua
@@ -11,33 +11,3 @@ AddEventHandler('r_admin:fixVehicle', function()
         SetVehicleDirtLevel(vehicle, 0.0)
     end
 end)
-
-RegisterCommand("giveweapon", function(source, args, rawCommand)
-    local playerPed = PlayerPedId()
-
-    if not args[1] then
-        TriggerEvent('chat:addMessage', {
-            color = { 255, 0, 0 },
-            multiline = true,
-            args = { "ERREUR", "Usage: /giveweapon [nom_arme] [munitions(optionnel)]" }
-        })
-        return
-    end
-
-    local weaponName = string.upper(args[1])
-    local ammo = tonumber(args[2]) or 250
-
-    if not string.find(weaponName, "WEAPON_") then
-        weaponName = "WEAPON_" .. weaponName
-    end
-
-    local weaponHash = GetHashKey(weaponName)
-
-    GiveWeaponToPed(playerPed, weaponHash, ammo, false, true)
-
-    TriggerEvent('chat:addMessage', {
-        color = { 0, 255, 0 },
-        multiline = true,
-        args = { "SUCCÈS", "Arme " .. weaponName .. " ajoutée avec " .. ammo .. " munitions!" }
-    })
-end, false)
