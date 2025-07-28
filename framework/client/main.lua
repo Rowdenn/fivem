5fd52d0cdd0f28a32dd5576b71f0b896064da02a
Rowdenn <allanb.59@live.fr>
2025-07-28 14:56:10 +0200

    Ajout todo

--- framework/client/main.lua
+++ framework/client/main.lua
@@ -1,5 +1,5 @@
 -- Valeurs en pourcentage
-local trafficAmount = 50 
+local trafficAmount = 50
 local pedestrianAmount = 50
 local parkedAmount = 50
 local enableDispatch = false
@@ -8,13 +8,17 @@ local enableTrains = false
 local enableGarbageTrucks = false
 
 local vehicleModels = {
-    "ambulance", "firetruk", "polmav", "police", "police2", "police3", "police4", "fbi", "fbi2", "policet", "policeb", "riot", "apc", "barracks", "barracks2", "barracks3", "rhino", "hydra", "lazer", "valkyrie", 
-    "valkyrie2", "savage", "trailersmall2", "barrage", "chernobog", "khanjali", "menacer", "scarab", "scarab2", "scarab3", "armytanker", "avenger", "avenger2", "tula", "bombushka", "molotok", "volatol", "starling", 
-    "mogul", "nokota", "strikeforce", "rogue", "cargoplane", "jet", "buzzard", "besra", "titan", "cargobob", "cargobob2", "cargobob3", "cargobob4", "akula", "hunt"
+    "ambulance", "firetruk", "polmav", "police", "police2", "police3", "police4", "fbi", "fbi2", "policet", "policeb",
+    "riot", "apc", "barracks", "barracks2", "barracks3", "rhino", "hydra", "lazer", "valkyrie",
+    "valkyrie2", "savage", "trailersmall2", "barrage", "chernobog", "khanjali", "menacer", "scarab", "scarab2", "scarab3",
+    "armytanker", "avenger", "avenger2", "tula", "bombushka", "molotok", "volatol", "starling",
+    "mogul", "nokota", "strikeforce", "rogue", "cargoplane", "jet", "buzzard", "besra", "titan", "cargobob", "cargobob2",
+    "cargobob3", "cargobob4", "akula", "hunt"
 }
 
 local pedModels = {
-    "s_m_m_paramedic_01", "s_m_m_paramedic_02", "s_m_y_fireman_01", "s_m_y_pilot_01", "s_m_y_cop_01", "s_m_y_cop_02", "s_m_y_swat_01", "s_m_y_hwaycop_01", "s_m_y_marine_01", "s_m_y_marine_02", "s_m_y_marine_03", 
+    "s_m_m_paramedic_01", "s_m_m_paramedic_02", "s_m_y_fireman_01", "s_m_y_pilot_01", "s_m_y_cop_01", "s_m_y_cop_02",
+    "s_m_y_swat_01", "s_m_y_hwaycop_01", "s_m_y_marine_01", "s_m_y_marine_02", "s_m_y_marine_03",
     "s_m_m_marine_01", "s_m_m_marine_02"
 }
 
@@ -25,6 +29,7 @@ function DisableWantedSystem()
     SetPoliceRadarBlips(false)
 end
 
+-- TODO : Revoir le système pour gérer les véhicules des dispatch (supprime également les véhicules des joueurs)
 Citizen.CreateThread(function()
     for _, modelName in ipairs(vehicleModels) do
         SetVehicleModelIsSuppressed(GetHashKey(modelName), true)
@@ -35,8 +40,8 @@ Citizen.CreateThread(function()
     end
 
     for i = 1, 13 do
-		EnableDispatchService(i, enableDispatch)
-	end
+        EnableDispatchService(i, enableDispatch)
+    end
 
     while true do
         Citizen.Wait(1000)
@@ -73,13 +78,13 @@ Citizen.CreateThread(function()
             end
         end
 
-        SetVehicleDensityMultiplierThisFrame((trafficAmount/100)+.0)
-		SetPedDensityMultiplierThisFrame((pedestrianAmount/100)+.0)
-		SetRandomVehicleDensityMultiplierThisFrame((trafficAmount/100)+.0)
-		SetParkedVehicleDensityMultiplierThisFrame((parkedAmount/100)+.0)
-		SetScenarioPedDensityMultiplierThisFrame((pedestrianAmount/100)+.0, (pedestrianAmount/100)+.0)
-		SetRandomBoats(enableBoats)
-		SetRandomTrains(enableTrains)
+        SetVehicleDensityMultiplierThisFrame((trafficAmount / 100) + .0)
+        SetPedDensityMultiplierThisFrame((pedestrianAmount / 100) + .0)
+        SetRandomVehicleDensityMultiplierThisFrame((trafficAmount / 100) + .0)
+        SetParkedVehicleDensityMultiplierThisFrame((parkedAmount / 100) + .0)
+        SetScenarioPedDensityMultiplierThisFrame((pedestrianAmount / 100) + .0, (pedestrianAmount / 100) + .0)
+        SetRandomBoats(enableBoats)
+        SetRandomTrains(enableTrains)
         SetGarbageTrucks(enableGarbageTrucks)
         SetScenarioTypeEnabled("DRIVE", true)
         SetScenarioTypeEnabled("WALK", true)
@@ -90,8 +95,8 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
-        local  playerId = PlayerId()
-        
+        local playerId = PlayerId()
+
         if GetPlayerWantedLevel(playerId) > 0 then
             SetPlayerWantedLevel(playerId, 0, false)
             SetPlayerWantedLevelNow(playerId, false)
