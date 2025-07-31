7b5adb33ae95f02e7d61e619c2efb4af7c965818
Rowdenn <allanb.59@live.fr>
2025-07-31 18:54:46 +0200

    Fonction pour récup le fullname d'un personnage

--- framework/server/core/players.lua
+++ framework/server/core/players.lua
@@ -14,3 +14,26 @@ function Framework.Player:GetPermissionsLevel(source)
 
     return 0
 end
+
+function Framework.Player:GetFullname(source, cb)
+    local identifier = GetPlayerIdentifier(source, 0)
+    if not identifier then return 0 end
+
+    Framework.Database:QuerySingle("SELECT firstname, lastname FROM users WHERE identifer = ?", { identifier },
+        function(result)
+            if result then
+                cb({
+                    identifier = identifier,
+                    firstname = result.firstname,
+                    lastname = result.lastname,
+                    fullname = result.firstname .. ' ' .. result.lastname
+                })
+            else
+                cb(nil)
+            end
+        end)
+
+
+
+    return nil
+end
