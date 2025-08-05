3de3044df9a92c5f8a7bc772152a8819d0fd900f
Rowdenn <allanb.59@live.fr>
2025-08-05 16:32:10 +0200

    Ajout de la config de banque dans la config globale

--- framework/shared/config.lua
+++ framework/shared/config.lua
@@ -58,14 +58,25 @@ GlobalConfig.Inventory.UsableItems = {
 GlobalConfig.Bank = {}
 
 GlobalConfig.Bank.ATMs = {
-    { coords = vector3(236.515, 219.642, 106.286) },
+    { coords = vec3(236.515, 219.642, 106.286) },
 }
 
 GlobalConfig.Bank.Banks = {
     {
         name = 'Banque de Vinewood',
-        coords = vector3(253.987, 222.464, 106.286),
-        heading = 160.979,
-        ped = "a_m_y_business_01"
+        coords = vec3(253.987, 222.464, 106.286),
+    },
+}
+
+GlobalConfig.Bank.Bankers = {
+    {
+        coords = vec3(253.980, 222.509, 106.287),
+        heading = 163.230,
+        model = "a_m_y_business_01"
+    },
+    {
+        coords = vec3(248.816, 224.372, 106.287),
+        heading = 159.282,
+        model = "a_m_y_business_01"
     },
 }
